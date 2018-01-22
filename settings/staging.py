54221739503e58d94c97931fa4638ed2f1d921c9
francesco75 <frscollo@hotmail.it>
2018-01-22 19:32:45 +0000

    Add files via upload

--- settings/staging.py
+++ settings/staging.py
@@ -1,11 +1,12 @@
 from base import *
-
+import dj_database_url
 DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        dj_database_url.config('CLEARDB_DATABASE_URL')
     }
 }
 

ad9497cf554afd8f1cb0068ad6cd6fe8e7ef32a8
francesco75 <frscollo@hotmail.it>
2018-01-28 19:41:00 +0000

    Add files via upload

---
+++ settings/staging.py
@@ -0,0 +1,37 @@
+
+from base import *
+#import dj_database_url
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #dj_database_url.config('CLEARDB_DATABASE_URL'),
+    }
+}
+
+
+# Paypal environment variables
+PAYPAL_NOTIFY_URL = 'https://project3-unicorn.herokuapp.com'
+PAYPAL_RECEIVER_EMAIL = 'frscollo@hotmail.com'
+
+SITE_URL = 'https://project3-unicorn.herokuapp.com'
+ALLOWED_HOSTS.append('project3-unicorn.herokuapp.com')
+
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}

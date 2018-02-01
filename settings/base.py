18737efaeee7b1745c80bb2d93002f93d7d248b4
francesco75 <frscollo@hotmail.it>
2018-02-01 01:19:47 +0000

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.whitenoise',
+    'whitenoise',
     'home',
     'accounts',
      'django.contrib.sites',
